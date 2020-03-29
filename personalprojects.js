edf5bb91e2fa5023741f2ba879e66b760a273eaa
emily <mle4d@me.com>
2020-03-28 20:20:48 -0700

    personal project JS start

---
+++ personalprojects.js
@@ -0,0 +1,10 @@
+document.querySelector('.ballPC').addEventListener('click', function() {
+  document.querySelector('#poster').classList.add('.flashbak');
+  console.log('click');
+  window.body
+});
+
+// function myFunction() {
+//   var element = document.getElementById("myDIV");
+//   element.classList.remove("mystyle");
+// }
