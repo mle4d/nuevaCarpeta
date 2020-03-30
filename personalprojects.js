b49bc03fb6ff69dfa946f0f9ce86fc9648dee861
emily <mle4d@me.com>
2020-03-29 20:40:39 -0700

    js to toggle class to display all the project posters

--- personalprojects.js
+++ personalprojects.js
@@ -1,6 +1,20 @@
 document.querySelector('.ballPC').addEventListener('click', function() {
   document.querySelector('.poster').classList.toggle('ballPoster');
-  console.log('click');
   window.body
 });
-
+document.querySelector('.nintiesPC').addEventListener('click', function() {
+  document.querySelector('.poster').classList.toggle('nintiesPoster');
+  window.body
+});
+document.querySelector('.meowPC').addEventListener('click', function() {
+  document.querySelector('.poster').classList.toggle('meowPoster');
+  window.body
+});
+document.querySelector('.bookshelfPC').addEventListener('click', function() {
+  document.querySelector('.poster').classList.toggle('bookshelfPoster');
+  window.body
+});
+document.querySelector('.spacePC').addEventListener('click', function() {
+  document.querySelector('.poster').classList.toggle('spacePoster');
+  window.body
+});
