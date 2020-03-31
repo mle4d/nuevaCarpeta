be8921b49f39417fc380c6be8a35a9148ac55d18
emily <mle4d@me.com>
2020-03-30 17:41:55 -0700

    scroll in space repo gif switch & tab link

--- personalprojects.js
+++ personalprojects.js
@@ -45,6 +45,9 @@ document.querySelector('.bookshelfPC').addEventListener('click', function() {
   document.querySelector('.screen').style.background = 'url(assets/bookshelfdesktop.png)';
   document.querySelector('.dtss').style.display = 'none';
   document.querySelector('.sticky').innerHTML = '<a class="stickyLink" href="https://top100booksof1989.netlify.com/" target="_blank">Visit Site</a>';
+  document.querySelector('.LTscreen').style.background = 'url(assets/bookshelfrepo.gif) center center';
+  document.querySelector('.circles').style.display = 'none';
+  document.querySelector('.tab').innerHTML = '<a class="tabLink" href="https://github.com/mle4d/js3017" target="_blank">See Code</a>';
   window.body
 });
 document.querySelector('.spacePC').addEventListener('click', function() {
@@ -55,5 +58,8 @@ document.querySelector('.spacePC').addEventListener('click', function() {
   document.querySelector('.screen').style.background = 'url(assets/sisdemo.gif)';
   document.querySelector('.dtss').style.display = 'none';
   document.querySelector('.sticky').innerHTML = '<a class="stickyLink" href="https://scrollinspace.netlify.com/" target="_blank">Visit Site</a>';
+  document.querySelector('.LTscreen').style.background = 'url(assets/sisrepo.gif) center center';
+  document.querySelector('.circles').style.display = 'none';
+  document.querySelector('.tab').innerHTML = '<a class="tabLink" href="https://github.com/mle4d/js3013" target="_blank">See Code</a>';
   window.body
 });
