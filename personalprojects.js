75ec922a28eef1f3f74fd53dd382ca5188b03195
emily <mle4d@me.com>
2020-03-30 19:28:03 -0700

    size background gifs to fit on switch

--- personalprojects.js
+++ personalprojects.js
@@ -4,9 +4,11 @@ document.querySelector('.ballPC').addEventListener('click', function() {
   document.querySelector('#info').innerHTML = '	○CSS Animations <br>○CSS Made Image<br>○Speech Recognition<br>○Randomized Array Function';
   document.querySelector('#art').style.display = 'none';
   document.querySelector('.screen').style.background = 'url(assets/c8bgif.gif)';
+  document.querySelector('.screen').style.backgroundSize = '100%';
   document.querySelector('.dtss').style.display = 'none';
   document.querySelector('.sticky').innerHTML = '<a class="stickyLink" href="https://cosmic8ball.netlify.com/" target="_blank">Visit Site</a>';
-  document.querySelector('.LTscreen').style.background = 'url(assets/c8ballrepo.gif) center center';
+  document.querySelector('.LTscreen').style.background = 'url(assets/c8ballrepo.gif)';
+  document.querySelector('.LTscreen').style.backgroundSize = '100%';
   document.querySelector('.circles').style.display = 'none';
   document.querySelector('.tab').innerHTML = '<a class="tabLink" href="https://github.com/mle4d/js3020" target="_blank">See Code</a>';
   window.body
@@ -17,9 +19,11 @@ document.querySelector('.nintiesPC').addEventListener('click', function() {
   document.querySelector('#info').innerHTML = '	○Custom Images <br>○Mouse follow image cursor<br>○Speech Recognition<br>○Speech Synthesis';
   document.querySelector('#art').style.display = 'none';
   document.querySelector('.screen').style.background = 'url(assets/90sfbgif.gif)';
+  document.querySelector('.screen').style.backgroundSize = '100%';
   document.querySelector('.dtss').style.display = 'none';
   document.querySelector('.sticky').innerHTML = '<a class="stickyLink" href="https://90sflashbak.netlify.com/" target="_blank">Visit Site</a>';
-  document.querySelector('.LTscreen').style.background = 'url(assets/90srepo.gif) center center';
+  document.querySelector('.LTscreen').style.background = 'url(assets/90srepo.gif)';
+  document.querySelector('.LTscreen').style.backgroundSize = '100%';
   document.querySelector('.circles').style.display = 'none';
   document.querySelector('.tab').innerHTML = '<a class="tabLink" href="https://github.com/mle4d/js3023" target="_blank">See Code</a>';
   window.body
@@ -29,10 +33,12 @@ document.querySelector('.meowPC').addEventListener('click', function() {
   document.querySelector('#info').style.color = '#ffffff';
   document.querySelector('#info').innerHTML = '	○Built with React <br>○Cute<br>○Meows<br>○Confuses Real Cats';
   document.querySelector('#art').style.display = 'none';
-  document.querySelector('.screen').style.background = 'url(assets/mmdesktop.png) top center';
+  document.querySelector('.screen').style.background = 'url(assets/mmdesktop.png)';
+  document.querySelector('.screen').style.backgroundSize = '100%';
   document.querySelector('.dtss').style.display = 'none';
   document.querySelector('.sticky').innerHTML = '<a class="stickyLink" href="https://meowmachine.netlify.com/" target="_blank">Visit Site</a>';
-  document.querySelector('.LTscreen').style.background = 'url(assets/mmrepo.gif) top center';
+  document.querySelector('.LTscreen').style.background = 'url(assets/mmrepo.gif)';
+  document.querySelector('.LTscreen').style.backgroundSize = '100%';
   document.querySelector('.circles').style.display = 'none';
   document.querySelector('.tab').innerHTML = '<a class="tabLink" href="https://github.com/mle4d/MeowMachine" target="_blank">See Code</a>';
   window.body
@@ -43,9 +49,11 @@ document.querySelector('.bookshelfPC').addEventListener('click', function() {
   document.querySelector('#info').innerHTML = '100 item sorted array styled with CSS using the nth child selector';
   document.querySelector('#art').style.display = 'none';
   document.querySelector('.screen').style.background = 'url(assets/bookshelfdesktop.png)';
+  document.querySelector('.screen').style.backgroundSize = '100%';
   document.querySelector('.dtss').style.display = 'none';
   document.querySelector('.sticky').innerHTML = '<a class="stickyLink" href="https://top100booksof1989.netlify.com/" target="_blank">Visit Site</a>';
-  document.querySelector('.LTscreen').style.background = 'url(assets/bookshelfrepo.gif) center center';
+  document.querySelector('.LTscreen').style.background = 'url(assets/bookshelfrepo.gif)';
+  document.querySelector('.LTscreen').style.backgroundSize = '100%';
   document.querySelector('.circles').style.display = 'none';
   document.querySelector('.tab').innerHTML = '<a class="tabLink" href="https://github.com/mle4d/js3017" target="_blank">See Code</a>';
   window.body
@@ -56,9 +64,11 @@ document.querySelector('.spacePC').addEventListener('click', function() {
   document.querySelector('#info').innerHTML = '	○Slide in on Scroll Function<br>○CSS Animations<br>○Customized Images';
   document.querySelector('#art').style.display = 'none';
   document.querySelector('.screen').style.background = 'url(assets/sisdemo.gif)';
+  document.querySelector('.screen').style.backgroundSize = '100%';
   document.querySelector('.dtss').style.display = 'none';
   document.querySelector('.sticky').innerHTML = '<a class="stickyLink" href="https://scrollinspace.netlify.com/" target="_blank">Visit Site</a>';
-  document.querySelector('.LTscreen').style.background = 'url(assets/sisrepo.gif) center center';
+  document.querySelector('.LTscreen').style.background = 'url(assets/sisrepo.gif)';
+  document.querySelector('.LTscreen').style.backgroundSize = '100%';
   document.querySelector('.circles').style.display = 'none';
   document.querySelector('.tab').innerHTML = '<a class="tabLink" href="https://github.com/mle4d/js3013" target="_blank">See Code</a>';
   window.body
