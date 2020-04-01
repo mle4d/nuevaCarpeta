6a4a39294eaef9d0c5ba01dff90d8851af2aba23
emily <mle4d@me.com>
2020-04-01 14:57:41 -0700

    logic for ATB images to go in place on click

--- collabproject.js
+++ collabproject.js
@@ -38,3 +38,23 @@ document.querySelector('.moddopc').addEventListener('click', function() {
   document.querySelector('.tab').innerHTML = '<a class="tabLink" href="https://github.com/rubber-ducky-squad/Adviser" target="_blank">See Code</a>';
   window.body
 });
+document.querySelector('.atbsticker').addEventListener('click', function() {
+  document.querySelector('.poster').style.background = 'url(assets/atbposter.png)';
+  document.querySelector('.poster').style.backgroundSize = 'cover';
+  document.querySelector('#info').style.color = 'white';
+  document.querySelector('#info').innerHTML = '';
+  document.querySelector('#art').style.display = 'none';
+  document.querySelector('.phone').style.background = 'url(assets/atbmobile.png)';
+  document.querySelector('.phone').style.backgroundSize = '100%';
+  document.querySelector('#clock').style.display = 'none';
+  document.querySelector('#date').style.display = 'none';
+  document.querySelector('.screen').style.background = 'url(assets/atbdemo.png)';
+  document.querySelector('.screen').style.backgroundSize = '100%';
+  document.querySelector('.dtss').style.display = 'none';
+  document.querySelector('.sticky').innerHTML = '<a class="stickyLink" href="https://twitter.com/AlchemyPDXBot" target="_blank">Visit Site</a>';
+  document.querySelector('.LTscreen').style.background = 'url(assets/atbrepo.gif)';
+  document.querySelector('.LTscreen').style.backgroundSize = '100%';
+  document.querySelector('.circles').style.display = 'none';
+  document.querySelector('.tab').innerHTML = '<a class="tabLink" href="https://github.com/rainbow-robots" target="_blank">See Code</a>';
+  window.body
+});
