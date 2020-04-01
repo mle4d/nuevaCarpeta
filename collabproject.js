e6d00a945cc6afb13541c48824ed4039a5621cba
emily <mle4d@me.com>
2020-04-01 14:15:55 -0700

    logic for all moddo imgages to be in place on click

--- collabproject.js
+++ collabproject.js
@@ -18,3 +18,23 @@ document.querySelector('.htsticker').addEventListener('click', function() {
   document.querySelector('.tab').innerHTML = '<a class="tabLink" href="https://github.com/happy-trees" target="_blank">See Code</a>';
   window.body
 });
+document.querySelector('.moddopc').addEventListener('click', function() {
+  document.querySelector('.poster').style.background = 'url(assets/moddoposter.png)';
+  document.querySelector('.poster').style.backgroundSize = 'cover';
+  document.querySelector('#info').style.color = 'white';
+  document.querySelector('#info').innerHTML = '';
+  document.querySelector('#art').style.display = 'none';
+  document.querySelector('.phone').style.background = 'url(assets/moddomobile.jpg)';
+  document.querySelector('.phone').style.backgroundSize = '100%';
+  document.querySelector('#clock').style.display = 'none';
+  document.querySelector('#date').style.display = 'none';
+  document.querySelector('.screen').style.background = 'url(assets/moddodemo.gif)';
+  document.querySelector('.screen').style.backgroundSize = '100%';
+  document.querySelector('.dtss').style.display = 'none';
+  document.querySelector('.sticky').innerHTML = '<a class="stickyLink" href="https://moddo.netlify.com/index.html" target="_blank">Visit Site</a>';
+  document.querySelector('.LTscreen').style.background = 'url(assets/moddorepo.gif)';
+  document.querySelector('.LTscreen').style.backgroundSize = '100%';
+  document.querySelector('.circles').style.display = 'none';
+  document.querySelector('.tab').innerHTML = '<a class="tabLink" href="https://github.com/rubber-ducky-squad/Adviser" target="_blank">See Code</a>';
+  window.body
+});
