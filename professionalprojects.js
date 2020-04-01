b9ff64e30d7bb476aa314ee254904b9526b9c7ab
emily <mle4d@me.com>
2020-03-31 18:11:27 -0700

    poster image switch on click

--- professionalprojects.js
+++ professionalprojects.js
@@ -6,3 +6,19 @@ document.querySelector('.meltmenu').addEventListener('click', function() {
   document.querySelector('#art').style.display = 'none';
   window.body
 });
+document.querySelector('.pkccFlyer').addEventListener('click', function() {
+  document.querySelector('.poster').style.background = 'url(assets/pkccposter.png)';
+  document.querySelector('.poster').style.backgroundSize = '100%';
+  document.querySelector('#info').style.color = 'white';
+  document.querySelector('#info').innerHTML = '	○<br>○<br>○<br>○';
+  document.querySelector('#art').style.display = 'none';
+  window.body
+});
+document.querySelector('.pdaaFlyer').addEventListener('click', function() {
+  document.querySelector('.poster').style.background = 'url(assets/pdaaposter.png)';
+  document.querySelector('.poster').style.backgroundSize = '100%';
+  document.querySelector('#info').style.color = 'white';
+  document.querySelector('#info').innerHTML = '	○<br>○<br>○<br>○';
+  document.querySelector('#art').style.display = 'none';
+  window.body
+});
