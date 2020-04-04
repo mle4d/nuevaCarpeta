ad43e3287c4d601a9980385d5461d6e08f7177dc
emily <mle4d@me.com>
2020-04-03 18:11:12 -0700

    make images no-repeat & friendly for different dimensions

--- collabproject.js
+++ collabproject.js
@@ -1,65 +1,65 @@
 document.querySelector('.htsticker').addEventListener('click', function() {
-  document.querySelector('.poster').style.background = 'url(assets/happytreesposter.png) center';
-  document.querySelector('.poster').style.backgroundSize = 'cover';
+  document.querySelector('.poster').style.background = 'url(assets/happytreesposter.png) center no-repeat #ffffff';
+  document.querySelector('.poster').style.backgroundSize = '110%';
   document.querySelector('#info').style.color = '#9b4fa7';
   document.querySelector('#info').innerHTML = 'Happy Trees is a fullstack multi-player drawing game built in five days on a team of four. Happy Trees uses React, Redux, Socket.io, P5.js, Bootstrap, Node, Express & MongoDB.';
   document.querySelector('#art').style.display = 'none';
-  document.querySelector('.phone').style.background = 'url(assets/happytreesmobile.jpg)';
+  document.querySelector('.phone').style.background = 'url(assets/happytreesmobile.jpg) center no-repeat #ffffff';
   document.querySelector('.phone').style.backgroundSize = '100%';
   document.querySelector('#clock').style.display = 'none';
   document.querySelector('#date').style.display = 'none';
-  document.querySelector('.screen').style.background = 'url(assets/happytreesdemo.gif)';
+  document.querySelector('.screen').style.background = 'url(assets/happytreesdemo.gif) #dedfe4 center no-repeat';
   document.querySelector('.screen').style.backgroundSize = '100%';
   document.querySelector('.dtss').style.display = 'none';
   document.querySelector('.sticky').innerHTML = '<a class="stickyLink" href="https://happy-trees.netlify.com/" target="_blank">Visit Site</a>';
-  document.querySelector('.LTscreen').style.background = 'url(assets/happytreesrepo.gif)';
+  document.querySelector('.LTscreen').style.background = 'url(assets/happytreesrepo.gif) #24272c center no-repeat';
   document.querySelector('.LTscreen').style.backgroundSize = '100%';
   document.querySelector('.circles').style.display = 'none';
   document.querySelector('.tab').innerHTML = '<a class="tabLink" href="https://github.com/happy-trees" target="_blank">See Code</a>';
   window.body
 });
 document.querySelector('.moddopc').addEventListener('click', function() {
-  document.querySelector('.poster').style.background = 'url(assets/moddoposter.png) center';
-  document.querySelector('.poster').style.backgroundSize = 'cover';
+  document.querySelector('.poster').style.background = 'url(assets/moddoposter.png) center no-repeat #fceff9';
+  document.querySelector('.poster').style.backgroundSize = '110%';
   document.querySelector('#info').style.color = '#8b7aa6';
   document.querySelector('#info').innerHTML = 'Moddo is a site to set your mood & daily motto of the day. This project was built in four days on a team five. This site uses two different APIs to combine for the randomly generated motto & implements firebase for the backend.';
   document.querySelector('#art').style.display = 'none';
-  document.querySelector('.phone').style.background = 'url(assets/moddomobile.jpg)';
+  document.querySelector('.phone').style.background = 'url(assets/moddomobile.jpg) center no-repeat #fceff9';
   document.querySelector('.phone').style.backgroundSize = '100%';
   document.querySelector('#clock').style.display = 'none';
   document.querySelector('#date').style.display = 'none';
-  document.querySelector('.screen').style.background = 'url(assets/moddodemo.gif)';
+  document.querySelector('.screen').style.background = 'url(assets/moddodemo.gif) #dedfe4 center no-repeat';
   document.querySelector('.screen').style.backgroundSize = '100%';
   document.querySelector('.dtss').style.display = 'none';
   document.querySelector('.sticky').innerHTML = '<a class="stickyLink" href="https://moddo.netlify.com/index.html" target="_blank">Visit Site</a>';
-  document.querySelector('.LTscreen').style.background = 'url(assets/moddorepo.gif)';
+  document.querySelector('.LTscreen').style.background = 'url(assets/moddorepo.gif) #24272c center no-repeat';
   document.querySelector('.LTscreen').style.backgroundSize = '100%';
   document.querySelector('.circles').style.display = 'none';
   document.querySelector('.tab').innerHTML = '<a class="tabLink" href="https://github.com/rubber-ducky-squad/Adviser" target="_blank">See Code</a>';
   window.body
 });
 document.querySelector('.atbsticker').addEventListener('click', function() {
-  document.querySelector('.poster').style.background = 'url(assets/atbposter.png) center';
-  document.querySelector('.poster').style.backgroundSize = 'cover';
+  document.querySelector('.poster').style.background = 'url(assets/atbposter.png) center no-repeat #ffffff';
+  document.querySelector('.poster').style.backgroundSize = '110%';
   document.querySelector('#info').style.color = '#458dc7';
   document.querySelector('#info').innerHTML = 'This twitter bot is a backend project built in four days on a team of three people. This bot is built with Node.js, MongoDB, Express, multiple NPM packages & incorporates a neotrellis keyboard hooked up to a Raspberry Pi to tweet emoji stories from.';
   document.querySelector('#art').style.display = 'none';
-  document.querySelector('.phone').style.background = 'url(assets/atbmobile.png)';
+  document.querySelector('.phone').style.background = 'url(assets/atbmobile.png) center no-repeat #ffffff';
   document.querySelector('.phone').style.backgroundSize = '100%';
   document.querySelector('#clock').style.display = 'none';
   document.querySelector('#date').style.display = 'none';
-  document.querySelector('.screen').style.background = 'url(assets/atbdemo.png)';
+  document.querySelector('.screen').style.background = 'url(assets/atbdemo.png) #dedfe4 center no-repeat';
   document.querySelector('.screen').style.backgroundSize = '100%';
   document.querySelector('.dtss').style.display = 'none';
   document.querySelector('.sticky').innerHTML = '<a class="stickyLink" href="https://twitter.com/AlchemyPDXBot" target="_blank">Visit Site</a>';
-  document.querySelector('.LTscreen').style.background = 'url(assets/atbrepo.gif)';
+  document.querySelector('.LTscreen').style.background = 'url(assets/atbrepo.gif) #24272c center no-repeat';
   document.querySelector('.LTscreen').style.backgroundSize = '100%';
   document.querySelector('.circles').style.display = 'none';
   document.querySelector('.tab').innerHTML = '<a class="tabLink" href="https://github.com/rainbow-robots" target="_blank">See Code</a>';
   window.body
 });
 document.querySelector('.sotostrip').addEventListener('click', function() {
-  document.querySelector('.poster').style.background = 'url(assets/sotoposter.png) center';
+  document.querySelector('.poster').style.background = 'url(assets/sotoposter.png) center no-repeat #ffffff';
   document.querySelector('.poster').style.backgroundSize = 'cover';
   document.querySelector('#info').style.color = '#000000';
   document.querySelector('#info').innerHTML = 'Sōto is a Vanilla JS Marie Kondo inspired sorting game built in four days on a team of three people.';
@@ -68,11 +68,11 @@ document.querySelector('.sotostrip').addEventListener('click', function() {
   document.querySelector('.phone').style.backgroundSize = '100%';
   document.querySelector('#clock').style.display = '';
   document.querySelector('#date').style.display = '';
-  document.querySelector('.screen').style.background = 'url(assets/sotodemo.gif)';
+  document.querySelector('.screen').style.background = 'url(assets/sotodemo.gif) #dedfe4 center no-repeat';
   document.querySelector('.screen').style.backgroundSize = '100%';
   document.querySelector('.dtss').style.display = 'none';
   document.querySelector('.sticky').innerHTML = '<a class="stickyLink" href="https://spark-joy.github.io/index.html" target="_blank">Visit Site</a>';
-  document.querySelector('.LTscreen').style.background = 'url(assets/sotorepo.gif)';
+  document.querySelector('.LTscreen').style.background = 'url(assets/sotorepo.gif) #24272c center no-repeat';
   document.querySelector('.LTscreen').style.backgroundSize = '100%';
   document.querySelector('.circles').style.display = 'none';
   document.querySelector('.tab').innerHTML = '<a class="tabLink" href="https://github.com/spark-joy/spark-joy.github.io" target="_blank">See Code</a>';
