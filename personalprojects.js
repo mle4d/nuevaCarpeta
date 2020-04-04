1c77c0db6b7af307890ef5e29818c6b3c79797bd
emily <mle4d@me.com>
2020-04-04 13:37:50 -0700

    gif desktop backgrounds to cover

--- personalprojects.js
+++ personalprojects.js
@@ -5,11 +5,11 @@ document.querySelector('.ballPC').addEventListener('click', function() {
   document.querySelector('#info').innerHTML = 'Cosmic 8 ball uses speech recognition so the user can ask a question outloud & the 8 ball answers using a randomized array. The 8 ball is made out of CSS & the site is jam packed with CSS animations.';
   document.querySelector('#art').style.display = 'none';
   document.querySelector('.screen').style.background = 'url(assets/c8bgif.gif) #dedfe4 center no-repeat';
-  document.querySelector('.screen').style.backgroundSize = '100%';
+  document.querySelector('.screen').style.backgroundSize = 'cover';
   document.querySelector('.dtss').style.display = 'none';
   document.querySelector('.sticky').innerHTML = '<a class="stickyLink" href="https://cosmic8ball.netlify.com/" target="_blank">Visit Site</a>';
   document.querySelector('.LTscreen').style.background = 'url(assets/c8ballrepo.gif) center #24272c no-repeat';
-  document.querySelector('.LTscreen').style.backgroundSize = '100%';
+  document.querySelector('.LTscreen').style.backgroundSize = 'cover';
   document.querySelector('.circles').style.display = 'none';
   document.querySelector('.tab').innerHTML = '<a class="tabLink" href="https://github.com/mle4d/js3020" target="_blank">See Code</a>';
   window.body
@@ -21,11 +21,11 @@ document.querySelector('.nintiesPC').addEventListener('click', function() {
   document.querySelector('#info').innerHTML = '90s flashbak is a nostaligic site using customized images & mouse-follow for a different user perspective. This site using speech recognition & speech synthesis for the user to interact with.';
   document.querySelector('#art').style.display = 'none';
   document.querySelector('.screen').style.background = 'url(assets/90sfbgif.gif) #dedfe4 center no-repeat';
-  document.querySelector('.screen').style.backgroundSize = '100%';
+  document.querySelector('.screen').style.backgroundSize = 'cover';
   document.querySelector('.dtss').style.display = 'none';
   document.querySelector('.sticky').innerHTML = '<a class="stickyLink" href="https://90sflashbak.netlify.com/" target="_blank">Visit Site</a>';
   document.querySelector('.LTscreen').style.background = 'url(assets/90srepo.gif) center #24272c no-repeat';
-  document.querySelector('.LTscreen').style.backgroundSize = '100%';
+  document.querySelector('.LTscreen').style.backgroundSize = 'cover';
   document.querySelector('.circles').style.display = 'none';
   document.querySelector('.tab').innerHTML = '<a class="tabLink" href="https://github.com/mle4d/js3023" target="_blank">See Code</a>';
   window.body
@@ -37,11 +37,11 @@ document.querySelector('.meowPC').addEventListener('click', function() {
   document.querySelector('#info').innerHTML = 'Meow Machine is the first site I ever made with React. It is nothing but cute & annoying, my cats hate it.';
   document.querySelector('#art').style.display = 'none';
   document.querySelector('.screen').style.background = 'url(assets/mmdesktop.png) #dedfe4 center no-repeat';
-  document.querySelector('.screen').style.backgroundSize = '100%';
+  document.querySelector('.screen').style.backgroundSize = 'cover';
   document.querySelector('.dtss').style.display = 'none';
   document.querySelector('.sticky').innerHTML = '<a class="stickyLink" href="https://meowmachine.netlify.com/" target="_blank">Visit Site</a>';
   document.querySelector('.LTscreen').style.background = 'url(assets/mmrepo.gif) center #24272c no-repeat';
-  document.querySelector('.LTscreen').style.backgroundSize = '100%';
+  document.querySelector('.LTscreen').style.backgroundSize = 'cover';
   document.querySelector('.circles').style.display = 'none';
   document.querySelector('.tab').innerHTML = '<a class="tabLink" href="https://github.com/mle4d/MeowMachine" target="_blank">See Code</a>';
   window.body
@@ -53,11 +53,11 @@ document.querySelector('.bookshelfPC').addEventListener('click', function() {
   document.querySelector('#info').innerHTML = 'This 1989 bookshelf is the top 100 books of the year put into a sorted array & styled with CSS using the nth child selector.';
   document.querySelector('#art').style.display = 'none';
   document.querySelector('.screen').style.background = 'url(assets/bookshelfdesktop.png) #dedfe4 center no-repeat';
-  document.querySelector('.screen').style.backgroundSize = '100%';
+  document.querySelector('.screen').style.backgroundSize = 'cover';
   document.querySelector('.dtss').style.display = 'none';
   document.querySelector('.sticky').innerHTML = '<a class="stickyLink" href="https://top100booksof1989.netlify.com/" target="_blank">Visit Site</a>';
   document.querySelector('.LTscreen').style.background = 'url(assets/bookshelfrepo.gif) center #24272c no-repeat';
-  document.querySelector('.LTscreen').style.backgroundSize = '100%';
+  document.querySelector('.LTscreen').style.backgroundSize = 'cover';
   document.querySelector('.circles').style.display = 'none';
   document.querySelector('.tab').innerHTML = '<a class="tabLink" href="https://github.com/mle4d/js3017" target="_blank">See Code</a>';
   window.body
@@ -69,11 +69,11 @@ document.querySelector('.spacePC').addEventListener('click', function() {
   document.querySelector('#info').innerHTML = 'Scroll in Space implements slide-in-on-scroll in a way I had not personally seen before. Customized images & CSS animations bring everything together.';
   document.querySelector('#art').style.display = 'none';
   document.querySelector('.screen').style.background = 'url(assets/sisdemo.gif) #dedfe4 center no-repeat';
-  document.querySelector('.screen').style.backgroundSize = '100%';
+  document.querySelector('.screen').style.backgroundSize = 'cover';
   document.querySelector('.dtss').style.display = 'none';
   document.querySelector('.sticky').innerHTML = '<a class="stickyLink" href="https://scrollinspace.netlify.com/" target="_blank">Visit Site</a>';
   document.querySelector('.LTscreen').style.background = 'url(assets/sisrepo.gif) center #24272c no-repeat';
-  document.querySelector('.LTscreen').style.backgroundSize = '100%';
+  document.querySelector('.LTscreen').style.backgroundSize = 'cover';
   document.querySelector('.circles').style.display = 'none';
   document.querySelector('.tab').innerHTML = '<a class="tabLink" href="https://github.com/mle4d/js3013" target="_blank">See Code</a>';
   window.body
