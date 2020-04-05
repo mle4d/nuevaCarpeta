9dfaec15f13528a4af0a4b995871663d49940771
emily <mle4d@me.com>
2020-04-05 14:01:00 -0700

    collab projects images update

--- collabproject.js
+++ collabproject.js
@@ -9,11 +9,11 @@ document.querySelector('.htsticker').addEventListener('click', function() {
   document.querySelector('#clock').style.display = 'none';
   document.querySelector('#date').style.display = 'none';
   document.querySelector('.screen').style.background = 'url(assets/happytreesdemo.gif) #dedfe4 center no-repeat';
-  document.querySelector('.screen').style.backgroundSize = '100%';
+  document.querySelector('.screen').style.backgroundSize = 'cover';
   document.querySelector('.dtss').style.display = 'none';
   document.querySelector('.sticky').innerHTML = '<a class="stickyLink" href="https://happy-trees.netlify.com/" target="_blank">Visit Site</a>';
   document.querySelector('.LTscreen').style.background = 'url(assets/happytreesrepo.gif) #24272c center no-repeat';
-  document.querySelector('.LTscreen').style.backgroundSize = '100%';
+  document.querySelector('.LTscreen').style.backgroundSize = 'cover';
   document.querySelector('.circles').style.display = 'none';
   document.querySelector('.tab').innerHTML = '<a class="tabLink" href="https://github.com/happy-trees" target="_blank">See Code</a>';
   window.body
@@ -29,11 +29,11 @@ document.querySelector('.moddopc').addEventListener('click', function() {
   document.querySelector('#clock').style.display = 'none';
   document.querySelector('#date').style.display = 'none';
   document.querySelector('.screen').style.background = 'url(assets/moddodemo.gif) #dedfe4 center no-repeat';
-  document.querySelector('.screen').style.backgroundSize = '100%';
+  document.querySelector('.screen').style.backgroundSize = 'cover';
   document.querySelector('.dtss').style.display = 'none';
   document.querySelector('.sticky').innerHTML = '<a class="stickyLink" href="https://moddo.netlify.com/index.html" target="_blank">Visit Site</a>';
   document.querySelector('.LTscreen').style.background = 'url(assets/moddorepo.gif) #24272c center no-repeat';
-  document.querySelector('.LTscreen').style.backgroundSize = '100%';
+  document.querySelector('.LTscreen').style.backgroundSize = 'cover';
   document.querySelector('.circles').style.display = 'none';
   document.querySelector('.tab').innerHTML = '<a class="tabLink" href="https://github.com/rubber-ducky-squad/Adviser" target="_blank">See Code</a>';
   window.body
@@ -49,11 +49,11 @@ document.querySelector('.atbsticker').addEventListener('click', function() {
   document.querySelector('#clock').style.display = 'none';
   document.querySelector('#date').style.display = 'none';
   document.querySelector('.screen').style.background = 'url(assets/atbdemo.png) #dedfe4 center no-repeat';
-  document.querySelector('.screen').style.backgroundSize = '100%';
+  document.querySelector('.screen').style.backgroundSize = 'cover';
   document.querySelector('.dtss').style.display = 'none';
   document.querySelector('.sticky').innerHTML = '<a class="stickyLink" href="https://twitter.com/AlchemyPDXBot" target="_blank">Visit Site</a>';
   document.querySelector('.LTscreen').style.background = 'url(assets/atbrepo.gif) #24272c center no-repeat';
-  document.querySelector('.LTscreen').style.backgroundSize = '100%';
+  document.querySelector('.LTscreen').style.backgroundSize = 'cover';
   document.querySelector('.circles').style.display = 'none';
   document.querySelector('.tab').innerHTML = '<a class="tabLink" href="https://github.com/rainbow-robots" target="_blank">See Code</a>';
   window.body
@@ -69,11 +69,11 @@ document.querySelector('.sotostrip').addEventListener('click', function() {
   document.querySelector('#clock').style.display = '';
   document.querySelector('#date').style.display = '';
   document.querySelector('.screen').style.background = 'url(assets/sotodemo.gif) #dedfe4 center no-repeat';
-  document.querySelector('.screen').style.backgroundSize = '100%';
+  document.querySelector('.screen').style.backgroundSize = 'cover';
   document.querySelector('.dtss').style.display = 'none';
   document.querySelector('.sticky').innerHTML = '<a class="stickyLink" href="https://spark-joy.github.io/index.html" target="_blank">Visit Site</a>';
   document.querySelector('.LTscreen').style.background = 'url(assets/sotorepo.gif) #24272c center no-repeat';
-  document.querySelector('.LTscreen').style.backgroundSize = '100%';
+  document.querySelector('.LTscreen').style.backgroundSize = 'cover';
   document.querySelector('.circles').style.display = 'none';
   document.querySelector('.tab').innerHTML = '<a class="tabLink" href="https://github.com/spark-joy/spark-joy.github.io" target="_blank">See Code</a>';
   window.body
