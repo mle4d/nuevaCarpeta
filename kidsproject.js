d47c91a5047fef7ae94f2a654074b2613a68bd80
emily <mle4d@me.com>
2020-06-28 19:42:45 -0700

    start JS for sonders superhero search

---
+++ kidsproject.js
@@ -0,0 +1,20 @@
+document.querySelector('.sssPC').addEventListener('click', function() {
+  document.querySelector('.poster').style.background = 'url(assets/.png) center no-repeat #ffffff';
+  document.querySelector('.poster').style.backgroundSize = '110%';
+  document.querySelector('#info').style.color = '';
+  document.querySelector('#info').innerHTML = 'Sonder\'s Superhero Search was my son\'s idea! The game features customized images & CSS/Javascript magic to create six rounds of superhero ssearching fun!';
+  document.querySelector('#art').style.display = 'none';
+  document.querySelector('.phone').style.background = 'url(assets/.jpg) center no-repeat #ffffff';
+  document.querySelector('.phone').style.backgroundSize = '100%';
+  document.querySelector('#clock').style.display = 'none';
+  document.querySelector('#date').style.display = 'none';
+  document.querySelector('.screen').style.background = 'url(assets/.gif) #dedfe4 center no-repeat';
+  document.querySelector('.screen').style.backgroundSize = 'cover';
+  document.querySelector('.dtss').style.display = 'none';
+  document.querySelector('.sticky').innerHTML = '<a class="stickyLink" href="https://sonderssuperherosearch.netlify.app/" target="_blank">Visit Site</a>';
+  document.querySelector('.LTscreen').style.background = 'url(assets/.gif) #24272c center no-repeat';
+  document.querySelector('.LTscreen').style.backgroundSize = 'cover';
+  document.querySelector('.circles').style.display = 'none';
+  document.querySelector('.tab').innerHTML = '<a class="tabLink" href="https://github.com/mle4d/SuperheroSearch" target="_blank">See Code</a>';
+  window.body
+});
