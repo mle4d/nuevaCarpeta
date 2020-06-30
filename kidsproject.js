b852feaa6a56b3dd0cee2ad549f587a49d205f8c
emily <mle4d@me.com>
2020-06-29 18:58:42 -0700

    lego build JS

--- kidsproject.js
+++ kidsproject.js
@@ -18,3 +18,23 @@ document.querySelector('.sssPC').addEventListener('click', function() {
   document.querySelector('.tab').innerHTML = '<a class="tabLink" href="https://github.com/mle4d/SuperheroSearch" target="_blank">See Code</a>';
   window.body
 });
+document.querySelector('.legoPC').addEventListener('click', function() {
+  document.querySelector('.poster').style.background = 'url(assets/.png) center no-repeat #ffffff';
+  document.querySelector('.poster').style.backgroundSize = '110%';
+  document.querySelector('#info').style.color = '';
+  document.querySelector('#info').innerHTML = 'Whack-A-Lego was the final build for the #Javascript30 Challenge. I made the challenge into a game for my son to play, & landed on LEGO as a theme for the challenge of the CSS.';
+  document.querySelector('#art').style.display = 'none';
+  document.querySelector('.phone').style.background = 'url(assets/.jpg) center no-repeat #ffffff';
+  document.querySelector('.phone').style.backgroundSize = '100%';
+  document.querySelector('#clock').style.display = 'none';
+  document.querySelector('#date').style.display = 'none';
+  document.querySelector('.screen').style.background = 'url(assets/.gif) #dedfe4 center no-repeat';
+  document.querySelector('.screen').style.backgroundSize = 'cover';
+  document.querySelector('.dtss').style.display = 'none';
+  document.querySelector('.sticky').innerHTML = '<a class="stickyLink" href="https://sonderssuperherosearch.netlify.app/" target="_blank">Visit Site</a>';
+  document.querySelector('.LTscreen').style.background = 'url(assets/.gif) #24272c center no-repeat';
+  document.querySelector('.LTscreen').style.backgroundSize = 'cover';
+  document.querySelector('.circles').style.display = 'none';
+  document.querySelector('.tab').innerHTML = '<a class="tabLink" href="https://github.com/mle4d/SuperheroSearch" target="_blank">See Code</a>';
+  window.body
+});
