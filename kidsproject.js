d64fd135bdda0371843a00835a4fd19cd70d833c
emily <mle4d@me.com>
2020-06-30 18:44:46 -0700

    toddler timer JS start

--- kidsproject.js
+++ kidsproject.js
@@ -38,3 +38,23 @@ document.querySelector('.legoPC').addEventListener('click', function() {
   document.querySelector('.tab').innerHTML = '<a class="tabLink" href="https://github.com/mle4d/SuperheroSearch" target="_blank">See Code</a>';
   window.body
 });
+document.querySelector('.timerPC').addEventListener('click', function() {
+  document.querySelector('.poster').style.background = 'url(assets/.png) center no-repeat #ffffff';
+  document.querySelector('.poster').style.backgroundSize = '110%';
+  document.querySelector('#info').style.color = '';
+  document.querySelector('#info').innerHTML = 'Todder Timer was made during the 2020 pandemic when my son & I were home all day, every day. Toddler timer helped manage simple chores & made my son excited to use it.';
+  document.querySelector('#art').style.display = 'none';
+  document.querySelector('.phone').style.background = 'url(assets/.jpg) center no-repeat #ffffff';
+  document.querySelector('.phone').style.backgroundSize = '100%';
+  document.querySelector('#clock').style.display = 'none';
+  document.querySelector('#date').style.display = 'none';
+  document.querySelector('.screen').style.background = 'url(assets/.gif) #dedfe4 center no-repeat';
+  document.querySelector('.screen').style.backgroundSize = 'cover';
+  document.querySelector('.dtss').style.display = 'none';
+  document.querySelector('.sticky').innerHTML = '<a class="stickyLink" href="https://toddlertimer.netlify.app/" target="_blank">Visit Site</a>';
+  document.querySelector('.LTscreen').style.background = 'url(assets/.gif) #24272c center no-repeat';
+  document.querySelector('.LTscreen').style.backgroundSize = 'cover';
+  document.querySelector('.circles').style.display = 'none';
+  document.querySelector('.tab').innerHTML = '<a class="tabLink" href="https://github.com/mle4d/js3029" target="_blank">See Code</a>';
+  window.body
+});
