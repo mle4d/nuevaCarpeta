aa331f867d2b704e7f455098ac340440ecb447bd
emily <mle4d@me.com>
2020-07-04 17:34:05 -0700

    insert lego repo gif

--- kidsproject.js
+++ kidsproject.js
@@ -32,7 +32,7 @@ document.querySelector('.legoPC').addEventListener('click', function() {
   document.querySelector('.screen').style.backgroundSize = 'cover';
   document.querySelector('.dtss').style.display = 'none';
   document.querySelector('.sticky').innerHTML = '<a class="stickyLink" href="https://whack-a-lego.netlify.app/" target="_blank">Visit Site</a>';
-  document.querySelector('.LTscreen').style.background = 'url(assets/.gif) #24272c center no-repeat';
+  document.querySelector('.LTscreen').style.background = 'url(assets/legorepo.gif) #24272c center no-repeat';
   document.querySelector('.LTscreen').style.backgroundSize = 'cover';
   document.querySelector('.circles').style.display = 'none';
   document.querySelector('.tab').innerHTML = '<a class="tabLink" href="https://github.com/mle4d/js3030" target="_blank">See Code</a>';
